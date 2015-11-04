24ed9345fffe409b7cfd8c69caa58207814f3195
Stefanie Yoshimura <syoshimura01@gmail.com>
2015-11-04 11:12:23 -0800

    Basic Files

---
+++ main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Stefanie on 11/4/2015.
+ */
+
+//Any global variable go here
+
+//Main board object
+
+//Board rendering function
+
+//Player tracking function
+
+//Win condition function
+
+//Reset function
+
+//Document Ready - should include basic click handler
+$(document).ready(function () {
+
+});
